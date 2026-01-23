15e39269858531504e4a181c0c082ad92462752b
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-01-22 22:59:05 -0300

    feat: adição de fotos na galeria

--- frontend/index.js
+++ frontend/index.js
@@ -104,7 +104,7 @@ ${mensagem}
 
   new ImageRotator({
     elementId: "slider",
-    images: ["./img/1.png", "./img/2.png"],
+    images: ["./img/1.png", "./img/2.png", "./img/3.png", "./img/4.png"],
     interval: 2000,
   });
 
