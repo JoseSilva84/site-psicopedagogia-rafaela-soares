2016b85a362a21b279e1ccd983314ee791f388a3
José Silva <juniornyanata@gmail.com>
2026-05-08 10:50:29 -0300

    feat: mudança nas fotos da apresentação

--- frontend/galeria.js
+++ frontend/galeria.js
@@ -5,7 +5,7 @@
 
 import { db, collection, getDocs, query, orderBy, onSnapshot } from './firebase-config.js';
 
-const FALLBACK = ['./img/1.png', './img/11.jpg'];
+const FALLBACK = ['./img/1.jpg', './img/2.jpg', './img/3.jpg'];
 
 async function loadInitial() {
   try {
